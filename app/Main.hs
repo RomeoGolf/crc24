c8cdb3989a49f2f5514c56e1826d485dc6270d7f
RomeoGolf <romeogolf@mail.ru>
2020-11-23 16:06:50 +0500

    refactor: Delete the useless code

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,9 @@ import Lib
 main :: IO ()
 main = do
     args <- getArgs
-    print . prefix . unwords $ args
     let fname = head args
     print fname
     content <- readFile fname
     print $ map (fst . head . readHex) (words content)
 
-prefix s = "Args:=> " ++ s
 
