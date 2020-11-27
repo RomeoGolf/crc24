5f4c47b273d9fdff9f16e6b0272782969f1bd5cb
RomeoGolf <romeogolf@mail.ru>
2020-11-27 11:22:58 +0500

    Add the draft for QuickCheck testing

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,25 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# LANGUAGE TemplateHaskell #-}
+
+import Test.QuickCheck
+
+import Ads_b
+
+import Data.Word (Word8, Word32)
+import Data.Bits ((.|.), (.&.), shift, xor)
+
+prop_reverseReverse :: [Int] -> Bool
+prop_reverseReverse xs = reverse (reverse xs) == xs
+
+prop_Crc24 :: [Word8] -> Bool
+prop_Crc24 xs = crc24 (x1:x2:x3:xs) == 0 where
+    crc' = crc24DataOnly xs
+    x1 = fromIntegral (crc' Data.Bits..&. 0xFF)
+    x2 = fromIntegral ((crc' `shift` (-8)) Data.Bits..&. 0xFF)
+    x3 = fromIntegral ((crc' `shift` (-16)) Data.Bits..&. 0xFF)
+
+
+return []
+{-main :: IO ()-}
+{-main = putStrLn "Test suite not yet implemented"-}
+main = $(quickCheckAll)
+{-main = quickCheck prop_Crc24-}
